7e0f7118a3bc48fc043da5f6e56bb234e1eb7293
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-26 13:15:27 +1100

    Initial commit

---
+++ display_cost_of_a_rectangle_plot_using_inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *  C# Program to Display Cost of a Rectangle Plot Using Inheritance
+ */
+
+using System;
+
+namespace display_cost_of_a_rectangle_plot_using_inheritance
+{
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+
+}
